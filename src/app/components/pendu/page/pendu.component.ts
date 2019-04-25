250be690d2d2e5e6d3d12840890e7aa7bcb98937
ACENSI\julien.lefur <julien.lefur@acensi.fr>
2019-04-25 11:31:54 +0200

    feat: create page pendu
    added hostlistener to get letter when user type keyboard
    added condition to reveal letter if it exists
    added counter for trys

--- src/app/components/pendu/page/pendu.component.ts
+++ src/app/components/pendu/page/pendu.component.ts
@@ -1,22 +1,40 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {PenduService} from '../services/pendu.service';
-import {Subscription} from 'rxjs';
+import {fromEvent, Observable, Subscription} from 'rxjs';
+import {map} from 'rxjs/operators';
+import index from '@angular/cli/lib/cli';
 
 @Component({
   selector: 'app-pendu',
   templateUrl: './pendu.component.html',
-  styleUrls: ['./pendu.component.sass']
+  styleUrls: ['./pendu.component.sass'],
 })
 export class PenduComponent implements OnInit, OnDestroy {
 
+
+
   sub: Subscription;
-  mots: any[] = [];
+  mots: string[] = [];
   randomMot: any;
+  motMystere: any[] = [];
+  essai: 0;
 
   constructor(
     // tslint:disable-next-line:variable-name
     private _penduService: PenduService
   ) { }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if (this.randomMot) {
+      this.essai += 1;
+      this.randomMot.mot.split('').forEach((lettre, i) => {
+        if (event.key === lettre) {
+          this.motMystere[i] = lettre;
+        }
+      });
+    }
+  }
 
   ngOnInit() {
     this.sub = this._penduService.getListOfMots()
@@ -31,8 +49,16 @@ export class PenduComponent implements OnInit, OnDestroy {
   }
 
   randomizeMot() {
-    this.randomMot = this.mots[Math.floor(Math.random() * this.mots.length)]
+    this.randomMot = this.mots[Math.floor(Math.random() * this.mots.length)];
     console.log('mon mot aléatoire: ', this.randomMot);
+    console.log('mon array de char est ', this.randomMot.mot.split(''));
+    for (const lettre of this.randomMot.mot.split('')) {
+      this.motMystere.push('?');
+    }
+    console.log('mon mot mystere est ', this.motMystere);
   }
 
+  chooseLetter($event: KeyboardEvent) {
+    console.log($event.key);
+  }
 }
